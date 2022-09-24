c7fcf7812ea05c15a61af7ef3b604f139c79e1cd
Alex Martens <alex@thinglab.org>
2022-09-24 08:07:18 -0700

    client: remove bash wrapper

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::{anyhow, Context};
 use nix_post_build_hook_queue_shared::SOCK_PATH;
-use std::os::unix::net::UnixDatagram;
+use std::os::unix::{net::UnixDatagram, prelude::OsStringExt};
 
 fn main() {
     // Never return errors
@@ -15,12 +15,17 @@ fn fallible_main() -> anyhow::Result<()> {
     sock.connect(SOCK_PATH)
         .with_context(|| format!("Failed to connect to socket at {SOCK_PATH}"))?;
 
-    for store_path in std::env::args().skip(1) {
-        println!("Sending to daemon: {store_path}");
+    let out_paths: Vec<u8> = std::env::var_os("OUT_PATHS")
+        .context("OUT_PATHS environment variable not set")?
+        .into_vec();
+
+    for store_path in out_paths.split(|ch| *ch == b' ') {
+        let store_path_utf8 = String::from_utf8_lossy(store_path);
+        println!("Sending to daemon: {store_path_utf8}");
         let n_bytes: usize = sock
-            .send(store_path.as_bytes())
-            .with_context(|| format!("Failed to write store path '{store_path}' to socket"))?;
-        let len: usize = store_path.as_bytes().len();
+            .send(store_path)
+            .with_context(|| format!("Failed to write store path '{store_path_utf8}' to socket"))?;
+        let len: usize = store_path.len();
         if n_bytes != len {
             return Err(anyhow!("Incomplete write, wrote {n_bytes} / {len} bytes"));
         }
