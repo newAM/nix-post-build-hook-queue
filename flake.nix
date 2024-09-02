36e596bb3ebd4d217dc6078473af5aad6afd884a
Alex Martens <alex@thinglab.org>
2024-09-02 10:32:26 -0700

    flake: remove unused input

--- flake.nix
+++ flake.nix
@@ -1,10 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    crane.url = "github:ipetkov/crane";
   };
 
   outputs = {
